bb670c7834c7cd168a2cac676bbfba2a24a98a8d
Ralph <ralph@thebrynards.com>
2021-11-10 10:56:52 -0600

    test account

---
+++ org/test/variable.tf
@@ -0,0 +1,10 @@
+variable "aws_account_friendly_name" {
+    description = "Friendly name for AWS account"
+    type = string
+    sensitive = true
+}
+variable "aws_account_email" {
+    description = "Account email address"
+    type = string
+    sensitive = true
+}
